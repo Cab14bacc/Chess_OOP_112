f71b775c48c256290146053a92f6f915a735191c
dkdoeoeo <timy3260@gmail.com>
2023-05-15 12:25:11 +0800

    手動合併

--- Chess_OOP/mainwindow.cpp
+++ Chess_OOP/mainwindow.cpp
@@ -3,6 +3,12 @@
 #include "ui_mainwindow.h"
 #include <sstream>
 #include <iostream>
+#include "King.h"
+#include "Queen.h"
+#include "Bishop.h"
+#include "Knight.h"
+#include "Rook.h"
+#include "Pawn.h"
 
 using namespace std;
 
@@ -67,12 +73,13 @@ void MainWindow::labelClicked()
     if(game.clickTimes == 1)
     {
         game.showCanMove(curRow, curCol);
-        game.clickTimes = 2;
+        printInformation();
         update();
     }
     else
     {
         game.playerMove(curRow, curCol);
+        printInformation();
         game.clickTimes = 1;
         update();
     }
@@ -80,7 +87,70 @@ void MainWindow::labelClicked()
 
 void MainWindow::update()
 {
+    for(int i = 0; i < 8; i++)
+    {
+        for(int j = 0; j < 8; j++)
+        {
+            MyLabel *lab;
+            QString row = QString::number(i);
+            QString col = QString::number(j);
+            lab = this->findChild<MyLabel*>(row + " " + col);
 
+            if(game.board[i][j].canMove)
+            {
+                lab->setStyleSheet("QLabel{background-color:rgb(255,253,85);}");
+            }
+            else
+            {
+                if((i + j) % 2 == 0)
+                {
+                    lab->setStyleSheet("QLabel{background-color:rgb(224,224,198);}");
+                }
+                else
+                {
+                    lab->setStyleSheet("QLabel{background-color:rgb(118,150,86);}");
+                }
+            }
+
+            if(game.board[i][j].ifHavePiece)
+            {
+                if(game.board[i][j].player == 'w')
+                {
+                    if(game.board[i][j].chessType == "Pawn")
+                        lab->setPixmap(*iconWPawn);
+                    else if(game.board[i][j].chessType == "Rook")
+                        lab->setPixmap(*iconWRook);
+                    else if(game.board[i][j].chessType == "Knight")
+                        lab->setPixmap(*iconWKnight);
+                    else if(game.board[i][j].chessType == "Bishop")
+                        lab->setPixmap(*iconWBishop);
+                    else if(game.board[i][j].chessType == "Queen")
+                        lab->setPixmap(*iconWQueen);
+                    else if(game.board[i][j].chessType == "King")
+                        lab->setPixmap(*iconWKing);
+                }
+                else//game.board[i][j].player == 'b'
+                {
+                    if(game.board[i][j].chessType == "Pawn")
+                        lab->setPixmap(*iconBPawn);
+                    else if(game.board[i][j].chessType == "Rook")
+                        lab->setPixmap(*iconBRook);
+                    else if(game.board[i][j].chessType == "Knight")
+                        lab->setPixmap(*iconBKnight);
+                    else if(game.board[i][j].chessType == "Bishop")
+                        lab->setPixmap(*iconBBishop);
+                    else if(game.board[i][j].chessType == "Queen")
+                        lab->setPixmap(*iconBQueen);
+                    else if(game.board[i][j].chessType == "King")
+                        lab->setPixmap(*iconBKing);
+                }
+            }
+            else
+            {
+                lab->clear();
+            }
+        }
+    }
 }
 
 // can ignore this temporary
@@ -102,6 +172,12 @@ void MainWindow::on_newGame_clicked()
     QString row;
     QString col;
     MyLabel *lab;
+    King *newKing = new King;
+    Queen *newQueen = new Queen;
+    Bishop *newBishop = new Bishop;
+    Knight *newKnight = new Knight;
+    Rook *newRook = new Rook;
+    Pawn *newPawn = new Pawn;
 
     for(int i = 0; i < 8; i++)
     {
@@ -109,76 +185,185 @@ void MainWindow::on_newGame_clicked()
         col = QString::number(i);
         lab = this->findChild<MyLabel*>(row + " " + col);
         lab->setPixmap(*iconBPawn);
+        game.Black.pawns.push_back(*newPawn);
+        game.board[1][i].player = 'b';
+        game.board[1][i].chessType = "Pawn";
+        game.board[1][i].ifHavePiece = true;
+        game.board[1][i].index = i;
         row = QString::number(6);
         col = QString::number(i);
         lab = this->findChild<MyLabel*>(row + " " + col);
         lab->setPixmap(*iconWPawn);
+        game.White.pawns.push_back(*newPawn);
+        game.board[6][i].player = 'w';
+        game.board[6][i].chessType = "Pawn";
+        game.board[6][i].ifHavePiece = true;
+        game.board[6][i].index = i;
     }
 
+    //set black rook
     row = QString::number(0);
     col = QString::number(0);
     lab = this->findChild<MyLabel*>(row + " " + col);
     lab->setPixmap(*iconBRook);
+    game.Black.rooks.push_back(*newRook);
+    game.board[0][0].player = 'b';
+    game.board[0][0].chessType = "Rook";
+    game.board[0][0].ifHavePiece = true;
+    game.board[0][0].index = 0;
+    row = QString::number(0);
+    col = QString::number(7);
+    lab = this->findChild<MyLabel*>(row + " " + col);
+    lab->setPixmap(*iconBRook);
+    game.Black.rooks.push_back(*newRook);
+    game.board[0][7].player = 'b';
+    game.board[0][7].chessType = "Rook";
+    game.board[0][7].ifHavePiece = true;
+    game.board[0][7].index = 1;
+
+    //set black Knight
     row = QString::number(0);
     col = QString::number(1);
     lab = this->findChild<MyLabel*>(row + " " + col);
     lab->setPixmap(*iconBKnight);
+    game.Black.knights.push_back(*newKnight);
+    game.board[0][1].player = 'b';
+    game.board[0][1].chessType = "Knight";
+    game.board[0][1].ifHavePiece = true;
+    game.board[0][1].index = 0;
+    row = QString::number(0);
+    col = QString::number(6);
+    lab = this->findChild<MyLabel*>(row + " " + col);
+    lab->setPixmap(*iconBKnight);
+    game.Black.knights.push_back(*newKnight);
+    game.board[0][6].player = 'b';
+    game.board[0][6].chessType = "Knight";
+    game.board[0][6].ifHavePiece = true;
+    game.board[0][6].index = 1;
+
+    //set black Bishop
     row = QString::number(0);
     col = QString::number(2);
     lab = this->findChild<MyLabel*>(row + " " + col);
     lab->setPixmap(*iconBBishop);
+    game.Black.bishops.push_back(*newBishop);
+    game.board[0][2].player = 'b';
+    game.board[0][2].chessType = "Bishop";
+    game.board[0][2].ifHavePiece = true;
+    game.board[0][2].index = 0;
+    row = QString::number(0);
+    col = QString::number(5);
+    lab = this->findChild<MyLabel*>(row + " " + col);
+    lab->setPixmap(*iconBBishop);
+    game.Black.bishops.push_back(*newBishop);
+    game.board[0][5].player = 'b';
+    game.board[0][5].chessType = "Bishop";
+    game.board[0][5].ifHavePiece = true;
+    game.board[0][5].index = 1;
+
+    //set black Queen
     row = QString::number(0);
     col = QString::number(3);
     lab = this->findChild<MyLabel*>(row + " " + col);
     lab->setPixmap(*iconBQueen);
+    game.Black.queens.push_back(*newQueen);
+    game.board[0][3].player = 'b';
+    game.board[0][3].chessType = "Queen";
+    game.board[0][3].ifHavePiece = true;
+    game.board[0][3].index = 0;
+
+    //set black King
     row = QString::number(0);
     col = QString::number(4);
     lab = this->findChild<MyLabel*>(row + " " + col);
     lab->setPixmap(*iconBKing);
-    row = QString::number(0);
-    col = QString::number(5);
-    lab = this->findChild<MyLabel*>(row + " " + col);
-    lab->setPixmap(*iconBBishop);
-    row = QString::number(0);
-    col = QString::number(6);
-    lab = this->findChild<MyLabel*>(row + " " + col);
-    lab->setPixmap(*iconBKnight);
-    row = QString::number(0);
-    col = QString::number(7);
-    lab = this->findChild<MyLabel*>(row + " " + col);
-    lab->setPixmap(*iconBRook);
+    game.board[0][4].player = 'b';
+    game.board[0][4].chessType = "King";
+    game.board[0][4].ifHavePiece = true;
+    game.board[0][4].index = 0;
+
+    //set white Rook
     row = QString::number(7);
     col = QString::number(0);
     lab = this->findChild<MyLabel*>(row + " " + col);
     lab->setPixmap(*iconWRook);
-    row = QString::number(7);
-    col = QString::number(1);
-    lab = this->findChild<MyLabel*>(row + " " + col);
-    lab->setPixmap(*iconWKnight);
-    row = QString::number(7);
-    col = QString::number(2);
-    lab = this->findChild<MyLabel*>(row + " " + col);
-    lab->setPixmap(*iconWBishop);
-    row = QString::number(7);
-    col = QString::number(3);
-    lab = this->findChild<MyLabel*>(row + " " + col);
-    lab->setPixmap(*iconWQueen);
-    row = QString::number(7);
-    col = QString::number(4);
-    lab = this->findChild<MyLabel*>(row + " " + col);
-    lab->setPixmap(*iconWKing);
-    row = QString::number(7);
-    col = QString::number(5);
-    lab = this->findChild<MyLabel*>(row + " " + col);
-    lab->setPixmap(*iconWBishop);
-    row = QString::number(7);
-    col = QString::number(6);
-    lab = this->findChild<MyLabel*>(row + " " + col);
-    lab->setPixmap(*iconWKnight);
+    game.White.rooks.push_back(*newRook);
+    game.board[7][0].player = 'w';
+    game.board[7][0].chessType = "Rook";
+    game.board[7][0].ifHavePiece = true;
+    game.board[7][0].index = 0;
     row = QString::number(7);
     col = QString::number(7);
     lab = this->findChild<MyLabel*>(row + " " + col);
     lab->setPixmap(*iconWRook);
+    game.White.rooks.push_back(*newRook);
+    game.board[7][7].player = 'w';
+    game.board[7][7].chessType = "Rook";
+    game.board[7][7].ifHavePiece = true;
+    game.board[7][7].index = 1;
+
+    //set white Knight
+    row = QString::number(7);
+    col = QString::number(1);
+    lab = this->findChild<MyLabel*>(row + " " + col);
+    lab->setPixmap(*iconWKnight);
+    game.White.knights.push_back(*newKnight);
+    game.board[7][1].player = 'w';
+    game.board[7][1].chessType = "Knight";
+    game.board[7][1].ifHavePiece = true;
+    game.board[7][1].index = 0;
+    row = QString::number(7);
+    col = QString::number(6);
+    lab = this->findChild<MyLabel*>(row + " " + col);
+    lab->setPixmap(*iconWKnight);
+    game.White.knights.push_back(*newKnight);
+    game.board[7][6].player = 'w';
+    game.board[7][6].chessType = "Knight";
+    game.board[7][6].ifHavePiece = true;
+    game.board[7][6].index = 1;
+
+    //set white Bishop
+    row = QString::number(7);
+    col = QString::number(2);
+    lab = this->findChild<MyLabel*>(row + " " + col);
+    lab->setPixmap(*iconWBishop);
+    game.White.bishops.push_back(*newBishop);
+    game.board[7][2].player = 'w';
+    game.board[7][2].chessType = "Bishop";
+    game.board[7][2].ifHavePiece = true;
+    game.board[7][2].index = 0;
+    row = QString::number(7);
+    col = QString::number(5);
+    lab = this->findChild<MyLabel*>(row + " " + col);
+    lab->setPixmap(*iconWBishop);
+    game.White.bishops.push_back(*newBishop);
+    game.board[7][5].player = 'w';
+    game.board[7][5].chessType = "Bishop";
+    game.board[7][5].ifHavePiece = true;
+    game.board[7][5].index = 1;
+
+    //set white Queen
+    row = QString::number(7);
+    col = QString::number(3);
+    lab = this->findChild<MyLabel*>(row + " " + col);
+    lab->setPixmap(*iconWQueen);
+    game.White.queens.push_back(*newQueen);
+    game.board[7][3].player = 'w';
+    game.board[7][3].chessType = "Queen";
+    game.board[7][3].ifHavePiece = true;
+    game.board[7][3].index = 0;
+
+    //set white King
+    row = QString::number(7);
+    col = QString::number(4);
+    lab = this->findChild<MyLabel*>(row + " " + col);
+    lab->setPixmap(*iconWKing);
+    game.board[7][4].player = 'w';
+    game.board[7][4].chessType = "King";
+    game.board[7][4].ifHavePiece = true;
+    game.board[7][4].index = 1;
+
+    printInformation();
 }
 
 //just to set images
@@ -208,4 +393,27 @@ void MainWindow::initIcon()
     *iconBKnight = iconBKnight->scaled(60,60);
     *iconBRook = iconBRook->scaled(60,60);
     *iconBPawn = iconBPawn->scaled(60,60);
+}
+
+void MainWindow::printInformation()
+{
+    system("cls");
+    for(int i = 0; i < 8; i++)
+    {
+        for(int j = 0; j < 8; j++)
+        {
+            if(game.board[i][j].ifHavePiece)
+            {
+                cout << game.board[i][j].player;
+                cout << game.board[i][j].chessType;
+            }
+
+            if(game.board[i][j].canMove)
+            {
+                cout << "c";
+            }
+            cout << i<<j<<" ";
+        }
+        cout << "\n";
+    }
 }
